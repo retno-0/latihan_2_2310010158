8dd4570dbdda0f327e6c5999a60f4fc8b26a4f56
retno-0 <retno-0>
2025-06-11 16:03:12 +0800

    initial commit

---
+++ src/latihan3/Rumus.java
@@ -0,0 +1,8 @@
+package latihan_3;
+    /*
+     * Nama : Retno
+     * NPM : 2310010158
+     * Kelas : 4C
+     */
+public class Rumus {
+}
--- src/latihan_2/DataDiriBeraksi.java
+++ src/latihan_2/DataDiriBeraksi.java
@@ -3,6 +3,5 @@ package latihan_2;
 public class DataDiriBeraksi {
     public static void main(String[] args) {
         DataDiri saya = new DataDiri("Retno", "2310010158", "4C TI BJB"); 
-        saya.tampilkanData();
     }
 }

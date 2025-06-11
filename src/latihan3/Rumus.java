1bce8ac01008e0e8490874e4280477dcb60af22a
retno-0 <retno-0>
2025-06-11 16:06:03 +0800

    menambahkan atribut

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -1,8 +1,12 @@
-package latihan_3;
+package latihan3;
     /*
      * Nama : Retno
      * NPM : 2310010158
      * Kelas : 4C
      */
 public class Rumus {
+    String sisi;
+    String panjang;
+    String lebar;
+    String hasil;
 }

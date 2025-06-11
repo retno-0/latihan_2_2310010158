3ef196739b7a6dfff513d77d38e5f32d1a9dae69
retno-0 <retno-0>
2025-06-11 15:28:03 +0800

    membuat package

---
+++ src/latihan_2/DataDiri.java
@@ -0,0 +1,13 @@
+package latihan_2;
+public class DataDiri {
+    String nama;
+    String npm;
+    String kelas;
+    
+public DataDiri(String nama, String npm, String kelas){
+     this.nama = nama;
+     this.npm = npm;
+     this.kelas = kelas;
+}
+}
+     
